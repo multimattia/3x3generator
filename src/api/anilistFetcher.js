5776bed26eb3295172472c09c061cb7f7fc0b3d8
maat <matt@mattchan.xyz>
2023-12-31 09:04:21 -0500

    Move data fetching logic to new file

--- src/api/anilistFetcher.js
+++ src/api/anilistFetcher.js
@@ -1,57 +1,51 @@
-// const getAnimeData = () => {
-//     const url = 'https://graphql.anilist.co';
-//     const query = `
-//       query ($username: String) {
-//         User (name: $username) {
-//           id
-//           name
-//           favourites {
-//             anime {
-//               nodes {
-//                 id
-//                 title {
-//                   english
-//                 }
-//                 coverImage {
-//                     extraLarge
-//                 }
-//               }
-//             }
-//             manga {
-//               nodes {
-//                 id
-//                 title {
-//                   english
-//                 }
-//                 coverImage {
-//                     extraLarge
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `;
+const getAnimeData = async (username) => {
+  const url = 'https://graphql.anilist.co';
+  const query = `
+      query ($username: String) {
+        User (name: $username) {
+          id
+          name
+          favourites {
+            anime {
+              nodes {
+                id
+                title {
+                  english
+                }
+                coverImage {
+                    extraLarge
+                }
+              }
+            }
+            manga {
+              nodes {
+                id
+                title {
+                  english
+                }
+                coverImage {
+                    extraLarge
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
 
-//     fetch(url, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json',
-//       },
-//       body: JSON.stringify({
-//         query,
-//         variables: {
-//           username: username,
-//         },
-//       }),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         data = data.data.User;
-//         console.log(`Username fetched: ${data.name}`);
-//         console.log('Anime returned:', data.favourites.anime.nodes);
-//         console.log(animeList);
-//       })
-//       .catch((error) => console.error('Error:', error));
-// }
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: {
+        username: username,
+      },
+    }),
+  }).then((response) => response.json());
+};
+
+export default getAnimeData;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,88 +1,40 @@
 import { useState } from 'react';
+import fetchAnimeData from '../api/anilistFetcher.js';
 
 const FormAndData = () => {
   const [username, setUsername] = useState('');
   const [error, setError] = useState(false);
   const [animeList, setAnimeList] = useState(
-    Array.from({ length: 9 }, (_, i) => ({ id: i + 1, name: null })),
+    Array.from({ length: 9 }, (_, i) => ({
+      id: `placeholder-${i}` + 1,
+      name: null,
+    })),
   );
 
   const handleUsernamechange = (e) => {
-    console.log(e.target.value);
     setUsername(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    console.log('handling submission');
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`Username: ${username}`);
-    const url = 'https://graphql.anilist.co';
-    const query = `
-      query ($username: String) {
-        User (name: $username) {
-          id
-          name
-          favourites {
-            anime {
-              nodes {
-                id
-                title {
-                  english
-                }
-                coverImage {
-                    extraLarge
-                }
-              }
-            }
-            manga {
-              nodes {
-                id
-                title {
-                  english
-                }
-                coverImage {
-                    extraLarge
-                }
-              }
-            }
-          }
-        }
-      }
-    `;
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        query,
-        variables: {
-          username: username,
-        },
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.errors) {
-          console.error('GraphQL Error:', data.errors);
-          setError(true);
-        } else if (data.data) {
-          setError(false);
-          data = data.data.User;
-          console.log(`Username fetched: ${data.name}`);
-          console.log('Anime returned:', data.favourites.anime.nodes);
-          setAnimeList(data.favourites.anime.nodes.slice(0, 9));
-          console.log(`Sliced animeList: ${animeList}`);
-        }
-      })
-      .catch((error) => console.error('Error:', error));
+    try {
+      const animeData = await fetchAnimeData(username);
+      setAnimeList(animeData.data.User.favourites.anime.nodes.slice(0, 9));
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
   };
 
   return (
     <div className='flex flex-col items-center justify-center align-middle'>
-      <form className='my-4' onSubmit={handleSubmit} action='GET'>
+      <form
+        className='my-4'
+        onSubmit={handleSubmit}
+        autoComplete='off'
+        action='GET'
+      >
         <span>
           <label htmlFor='username'>Enter username here:</label>
           <input
@@ -102,11 +54,7 @@ const FormAndData = () => {
         />
       </form>
       <div>
-        {error ? (
-          <p className='text-red-500'>Username does not exist</p>
-        ) : (
-          <p></p>
-        )}
+        {error ? <p className='text-red-500'>Username does not exist</p> : null}
       </div>
       <div className='grid grid-cols-3 gap-1 sm:gap-2'>
         {animeList.map((anime, index) =>
@@ -119,7 +67,7 @@ const FormAndData = () => {
             />
           ) : (
             <div
-              key={index}
+              key={anime.id}
               className='flex h-24 w-24 items-center justify-center bg-slate-200 object-cover sm:h-40 sm:w-40 md:h-64 md:w-64'
             >
               <p className='text-slate-400' key={index}>
