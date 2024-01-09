20bca1b2686de157bb922b619deb7003a4c8da50
maat <matt@mattchan.xyz>
2024-01-08 21:20:31 -0500

    Update title

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className='flex flex-col justify-center'>
-        <h1 className='text-3xl font-bold'>Anime 3x3 creator</h1>
+        <h1 className='text-3xl font-bold'>3x3 AniList Grid Creator</h1>
         <FormAndData />
       </div>
     </>
